a40a8c3af4016d643c3a514e8f798083e9dd80d4
facu <facu63829@gmail.com>
2020-11-04 14:44:39 -0300

    Admin Realizado

--- Buffet/models.py
+++ Buffet/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+#ESTADO_MESA = [
+#    ('LB', 'Libre'),
+#    ('RS', 'Reservada'),
+#    ('OP', 'Ocupada'),
+#    ('NO', 'No Disponible'),
+#]
+
 class Mesa(models.Model):
     num_Mesa = models.AutoField(primary_key=True)
-    cantSillas = models.IntegerField(max_length=6)
+    cantSillas = models.CharField(max_length=6)
+    #status = models.CharField(max_length=2, choices=ESTADO_MESA, default='Libre')
+
     def __str__(self):
         return 'Mesa: {}'.format(self.num_Mesa)
+
 
 class Cliente(models.Model):
     id = models.AutoField(primary_key=True)
@@ -16,11 +26,55 @@ class Cliente(models.Model):
     telefono = models.CharField(max_length=30)
     correo = models.CharField(max_length=40)
 
+    def __str__(self):
+        return "{} {}".format(self.nombre,self.apellido)
+
+
+
 class Reserva(models.Model):
     num_Res = models.AutoField(primary_key=True)
     fecha = models.DateField()
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
 
+
 class Pedido(models.Model):
     num_ped = models.AutoField(primary_key=True)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+
+class Pago(models.Model):
+    id = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    num_trj = models.CharField(max_length=20)
+    fechaV = models.DateField()
+    codSeg = models.CharField(max_length=5)
+
+    def __str__(self):
+        return "{}".format(self.cliente)
+
+
+class Bebida(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    #pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Platillo(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    # pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Acompaniante(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    # pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
